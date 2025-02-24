560d6bc0459419707c9a01aa91e608e6ff2ac732
Abhishek Pavithran <abhishekpavi6@gmail.com>
2025-02-24 14:42:58 +0530

    update

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -39,7 +39,14 @@ exports.login = async (req, res) => {
 
 exports.getProfile = async (req, res) => {
     try {
-        const user = await User.findById(req.userId).select("name email password _id");
+        // Selecting name, email, and password fields
+        const user = await User.findById(req.userId).select("name email password");
+        
+        // Checking if the user exists
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
         res.json(user);
     } catch (error) {
         res.status(500).json({ message: "Error fetching profile" });
